b7815de4d3e345a5ba0c1158e02c0308ccd247e3
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-27 09:28:30 +0300

    Update HeaderPage.jsx

--- src/pages/HeaderPage.jsx
+++ src/pages/HeaderPage.jsx
@@ -11,37 +11,35 @@ const HeaderPage = () =>
 export default HeaderPage;
 
 
-import React from 'react';
-import { ReactComponent as PrimeChatLogo } from './primechat-logo.svg';
+// import React from 'react';
+// import { ReactComponent as PrimeChatLogo } from './primechat-logo.svg';
 
-function HeaderPage() {
-  return (
-    <header>
-      <nav>
-        <div className="logo">
-          <PrimeChatLogo />
-        </div>
-        <ul className="nav-links">
-          <li>
-            <a href="#about-us">About Us</a>
-          </li>
-          <li>
-            <a href="#platform">Platform</a>
-          </li>
-          <li>
-            <a href="#resources">Resources</a>
-          </li>
-          <li>
-            <a href="#solution">Solution</a>
-          </li>
-          <li>
-            <a href="#pricing">Pricing</a>
-          </li>
-        </ul>
-        <button className="logout-btn">Logout</button>
-      </nav>
-    </header>
-  );
-}
-
-export default HeaderPage;
+// function HeaderPage() {
+//   return (
+//     <header>
+//       <nav>
+//         <div className="logo">
+//           <PrimeChatLogo />
+//         </div>
+//         <ul className="nav-links">
+//           <li>
+//             <a href="#about-us">About Us</a>
+//           </li>
+//           <li>
+//             <a href="#platform">Platform</a>
+//           </li>
+//           <li>
+//             <a href="#resources">Resources</a>
+//           </li>
+//           <li>
+//             <a href="#solution">Solution</a>
+//           </li>
+//           <li>
+//             <a href="#pricing">Pricing</a>
+//           </li>
+//         </ul>
+//         <button className="logout-btn">Logout</button>
+//       </nav>
+//     </header>
+//   );
+// }
