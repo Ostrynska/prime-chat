c143fb8e3d5957264659fea2d795e59e0ea7dfcf
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-03 20:25:56 +0300

    Update HeaderPage.jsx

--- src/pages/HeaderPage.jsx
+++ src/pages/HeaderPage.jsx
@@ -1,45 +1,35 @@
-const HeaderPage = () =>
-{
-    <div>
-        <nav>
-            <svg>
-            </svg>
-        </nav>
-    </div>
-}
+import { Container } from '../components/Container/Conteiner';
+
+import { MenuItem } from './Hero.styled';
+
+const HeaderPage = () => {
+ return (
+  <Container>
+   <header>
+    <nav>
+     <div></div>
+     <ul>
+      <MenuItem>
+       <a href="#about-us">About Us</a>
+      </MenuItem>
+      <MenuItem>
+       <a href="#platform">Platform</a>
+      </MenuItem>
+      <MenuItem>
+       <a href="#resources">Resources</a>
+      </MenuItem>
+      <MenuItem>
+       <a href="#solution">Solution</a>
+      </MenuItem>
+      <MenuItem>
+       <a href="#pricing">Pricing</a>
+      </MenuItem>
+     </ul>
+     <button>Logout</button>
+    </nav>
+   </header>
+  </Container>
+ );
+};
 
 export default HeaderPage;
-
-
-// import React from 'react';
-// import { ReactComponent as PrimeChatLogo } from './primechat-logo.svg';
-
-// function HeaderPage() {
-//   return (
-//     <header>
-//       <nav>
-//         <div className="logo">
-//           <PrimeChatLogo />
-//         </div>
-//         <ul className="nav-links">
-//           <li>
-//             <a href="#about-us">About Us</a>
-//           </li>
-//           <li>
-//             <a href="#platform">Platform</a>
-//           </li>
-//           <li>
-//             <a href="#resources">Resources</a>
-//           </li>
-//           <li>
-//             <a href="#solution">Solution</a>
-//           </li>
-//           <li>
-//             <a href="#pricing">Pricing</a>
-//           </li>
-//         </ul>
-//         <button className="logout-btn">Logout</button>
-//       </nav>
-//     </header>
-//   );
-// }
