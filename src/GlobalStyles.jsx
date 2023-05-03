c28173c9cddd93d7146167ac1eba7f0e9819550f
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-03 12:24:44 +0300

    Update GlobalStyles.jsx

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -1,46 +1,54 @@
 import { createGlobalStyle } from 'styled-components';
 import modernNormalize from 'modern-normalize';
-// import Poppins700 from '../Fonts/Poppins/Poppins-Bold.ttf';
-// import Poppins600 from '../Fonts/Poppins/Poppins-SemiBold.ttf';
-// import Poppins500 from '../Fonts/Poppins/Poppins-Medium.ttf';
-// import Poppins400 from '../Fonts/Poppins/Poppins-Regular.ttf';
+import Inter700 from '../fonts/Inter/Inter-Bold.ttf';
+import Inter600 from '../fonts/Inter/Inter-SemiBold.ttf';
+import Inter500 from '../fonts/Inter/Inter-Medium.ttf';
+import Inter400 from '../fonts/Inter/Inter-Regular.ttf';
+
+import OpenSans700 from '../fonts/OpenSans/OpenSans-Bold.ttf';
+import OpenSans600 from '../fonts/OpenSans/OpenSans-SemiBold.ttf';
+import OpenSans500 from '../fonts/OpenSans/OpenSans-Medium.ttf';
+import OpenSans400 from '../fonts/OpenSans/OpenSans-Regular.ttf';
 
 export const GlobalStyle = createGlobalStyle`
 @font-face {
-    font-family: 'Poppins';
-    ${'' /* src: url(${Poppins400}), url(${Poppins500}), url(${Poppins600}), url(${Poppins700}); */}
-    }
+  font-family: 'Inter';
+    src: url(${Inter400}), url(${Inter500}), url(${Inter600}), url(${Inter700})
+  }
+@font-face {
+  font-family: 'OpenSans';
+    src: url(${OpenSans400}), url(${OpenSans500}), url(${OpenSans600}), url(${OpenSans700})
+  }
 
   body {
-  margin: 0;
+    margin: 0;
     padding: 0;
- font-family: 'Poppins', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    font-family: 'OpenSans', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 
-   background-color: #FAFAFA;
-      color: rgb(1,67,97);
-      -webkit-tap-highlight-color: transparent;
+    background-color: #FAFAFA;
+    color: rgb(1,67,97);
+    -webkit-tap-highlight-color: transparent;
 }
-${modernNormalize}
+  ${modernNormalize}
 
-code {
- font-family: 'Poppins', sans-serif;
-}
+  code {
+  font-family: 'OpenSans', sans-serif;
+  }
 
-ul {
-  list-style: none;
-   margin: 0;
+  ul {
+    list-style: none;
+    margin: 0;
     padding: 0;
-}
+  }
 
- a {
+  a {
     text-decoration: none;
-   }
+  }
 
 
   h1, h2, h3, h4, h5, h6, p, button {
-
     margin: 0;
     padding: 0;
   }
@@ -50,15 +58,15 @@ ul {
   }
 `;
 
-// :root.themeLight {
-//     --bgColor: #FFFFFF;
-//     --bgSecondaryColor: #FFF3E1;
-//     --contentColor: #4e4e4e;
-//     --primaryColor: #625FFB;
-//     --secondaryColor: #E2992B;
-//     --headerColor: #303030;
-//     --titleColor: #263238;
-//     --textColor: #4E4E4E;
-//     --whiteColor: #FFFFFF;
-//     --greyColor: #F3F3F3;
-//     --darkGreyColor: #454545;
+:root.themeLight {
+    --bgColor: #FFFFFF;
+    --bgSecondaryColor: #FFF3E1;
+    --contentColor: #4e4e4e;
+    --primaryColor: #625FFB;
+    --secondaryColor: #E2992B;
+    --headerColor: #303030;
+    --titleColor: #263238;
+    --textColor: #4E4E4E;
+    --whiteColor: #FFFFFF;
+    --greyColor: #F3F3F3;
+    --darkGreyColor: #454545;
