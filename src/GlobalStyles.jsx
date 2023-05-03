e3dd0c304bc4e1ac321d76d7563de670e2054bba
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-03 20:25:11 +0300

    Update GlobalStyles.jsx

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -1,23 +1,59 @@
 import { createGlobalStyle } from 'styled-components';
 import modernNormalize from 'modern-normalize';
-import Inter700 from '../fonts/Inter/Inter-Bold.ttf';
-import Inter600 from '../fonts/Inter/Inter-SemiBold.ttf';
-import Inter500 from '../fonts/Inter/Inter-Medium.ttf';
-import Inter400 from '../fonts/Inter/Inter-Regular.ttf';
-
-import OpenSans700 from '../fonts/OpenSans/OpenSans-Bold.ttf';
-import OpenSans600 from '../fonts/OpenSans/OpenSans-SemiBold.ttf';
-import OpenSans500 from '../fonts/OpenSans/OpenSans-Medium.ttf';
-import OpenSans400 from '../fonts/OpenSans/OpenSans-Regular.ttf';
 
 export const GlobalStyle = createGlobalStyle`
 @font-face {
   font-family: 'Inter';
-    src: url(${Inter400}), url(${Inter500}), url(${Inter600}), url(${Inter700})
+  src: url('./fonts/Inter/Inter-Regular.ttf') format('truetype');
+  font-weight: 400;
   }
+
+@font-face {
+  font-family: 'Inter';
+  src: url('./fonts/Inter/Inter-Medium.ttf') format('truetype');
+  font-weight: 500;
+  }
+
+@font-face {
+  font-family: 'Inter';
+  src: url('./fonts/Inter/Inter-SemiBold.ttf') format('truetype');
+  font-weight: 600;
+  }
+
+@font-face {
+  font-family: 'Inter';
+  src: url('./fonts/Inter/Inter-Bold.ttf') format('truetype');
+  font-weight: 700;
+  }
+
 @font-face {
   font-family: 'OpenSans';
-    src: url(${OpenSans400}), url(${OpenSans500}), url(${OpenSans600}), url(${OpenSans700})
+  src: url('./fonts/OpenSans/OpenSans-Light.ttf') format('truetype');
+  font-weight: 300;
+  }
+
+@font-face {
+  font-family: 'OpenSans';
+  src: url('./fonts/OpenSans/OpenSans-Medium.ttf') format('truetype');
+  font-weight: 400;
+  }
+
+@font-face {
+  font-family: 'OpenSans';
+  src: url('./fonts/OpenSans/OpenSans-Regular.ttf') format('truetype');
+  font-weight: 500;
+  }
+
+@font-face {
+  font-family: 'OpenSans';
+  src: url('./fonts/OpenSans/OpenSans-SemiBold.ttf') format('truetype');
+  font-weight: 600;
+  }
+
+@font-face {
+  font-family: 'OpenSans';
+  src: url('./fonts/OpenSans/OpenSans-Bold.ttf') format('truetype');
+  font-weight: 700;
   }
 
   body {
@@ -34,7 +70,7 @@ export const GlobalStyle = createGlobalStyle`
   ${modernNormalize}
 
   code {
-  font-family: 'OpenSans', sans-serif;
+    font-family: 'OpenSans', sans-serif;
   }
 
   ul {
@@ -47,6 +83,15 @@ export const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
 
+  button, input {
+    background: none;
+    color: inherit;
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+}
 
   h1, h2, h3, h4, h5, h6, p, button {
     margin: 0;
@@ -57,16 +102,3 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 `;
-
-:root.themeLight {
-    --bgColor: #FFFFFF;
-    --bgSecondaryColor: #FFF3E1;
-    --contentColor: #4e4e4e;
-    --primaryColor: #625FFB;
-    --secondaryColor: #E2992B;
-    --headerColor: #303030;
-    --titleColor: #263238;
-    --textColor: #4E4E4E;
-    --whiteColor: #FFFFFF;
-    --greyColor: #F3F3F3;
-    --darkGreyColor: #454545;
