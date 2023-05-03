35755d0a16acbb3bd8685568bd45a62333a8ffc6
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-03 12:24:38 +0300

    Create Hero.jsx

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import {Button} from "./Hero.styled"
+
+const Hero = () =>
+{
+    return (
+        <>
+            <div>
+                <p>Chatbot</p>
+            </div>
+            <h1>Offer The Best <span>Conversational</span> <span>Experience</span> With Our Primchat Software</h1>
+            <p>Provide excellent customer service. Answer more tickets all-in-one Primarily software.</p>
+            <Button>Get started</Button>
+            <Button>Book a demo</Button>
+        </>
+    )
+}
+
+export default Hero;
