73aade5a0727cd00a16467428c6208a7601bc153
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-04 16:40:04 +0300

    Update HeaderPage.jsx

--- src/pages/HeaderPage.jsx
+++ src/pages/HeaderPage.jsx
@@ -1,34 +1,10 @@
-import { Container } from '../components/Container/Conteiner';
-
-import { MenuItem } from './Hero.styled';
+import Header from '../components/Header/Header';
 
 const HeaderPage = () => {
  return (
-  <Container>
-   <header>
-    <nav>
-     <div></div>
-     <ul>
-      <MenuItem>
-       <a href="#about-us">About Us</a>
-      </MenuItem>
-      <MenuItem>
-       <a href="#platform">Platform</a>
-      </MenuItem>
-      <MenuItem>
-       <a href="#resources">Resources</a>
-      </MenuItem>
-      <MenuItem>
-       <a href="#solution">Solution</a>
-      </MenuItem>
-      <MenuItem>
-       <a href="#pricing">Pricing</a>
-      </MenuItem>
-     </ul>
-     <button>Logout</button>
-    </nav>
-   </header>
-  </Container>
+  <>
+   <Header />
+  </>
  );
 };
 
