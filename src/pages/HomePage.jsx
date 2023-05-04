6fdf250411536cf4d08c0700c48f707ae90b0d78
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-04 16:39:59 +0300

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Hero from '../components/Hero/Hero';
+import Partners from '../components/Partners/Partners';
+import { Container } from '../components/Container/Conteiner';
+
+const HomePage = () => {
+ return (
+  <Container>
+   <Hero />
+   <Partners />
+  </Container>
+ );
+};
+
+export default HomePage;
