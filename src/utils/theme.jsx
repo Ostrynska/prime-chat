adc40b5bb81dfad6bcecb5a9d17b8536205b7302
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-10 12:06:20 +0300

    Update theme.jsx

--- src/utils/theme.jsx
+++ src/utils/theme.jsx
@@ -32,5 +32,14 @@ export const lightTheme = {
   greyColor: '#F3F3F3',
   darkGreyColor: '#454545',
   lightGreyColor: '#F7F7F7',
+  checkOrange: '#FFB545',
+  inputOrange:
+   'linear-gradient(89.88deg, #FFEDD1 1.51%, rgba(255, 255, 255, 0) 84.54%)',
+  checkPurple: '#625FFB',
+  inputPurple:
+   'linear-gradient(89.88deg, #D3D2FF 1.51%, rgba(255, 255, 255, 0) 84.54%)',
+  checkGreen: '#65CB7B',
+  inputGreen:
+   'linear-gradient(89.88deg, #D6F4DC 1.51%, rgba(255, 255, 255, 0) 84.54%)',
  },
 };
