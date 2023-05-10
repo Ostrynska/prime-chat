3bc0ffa8b3c3b75efd8e661cd25e05a55a4819f8
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-10 20:28:52 +0300

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,22 +1,9 @@
-import {
- PrimaryButton,
- SecondaryButton,
- SecondaryButtonS,
- PrimaryButtonS,
-} from './Button.styled';
+import { PrimaryButton, SecondaryButton } from './Button.styled';
 
 export const Button = ({ text, variant = 'primary' }) => {
  return variant === 'secondary' ? (
   <SecondaryButton>{text}</SecondaryButton>
  ) : (
   <PrimaryButton>{text}</PrimaryButton>
- );
-};
-
-export const ButtonS = ({ text, variant = 'primary' }) => {
- return variant === 'secondary' ? (
-  <SecondaryButtonS>{text}</SecondaryButtonS>
- ) : (
-  <PrimaryButtonS>{text}</PrimaryButtonS>
  );
 };
