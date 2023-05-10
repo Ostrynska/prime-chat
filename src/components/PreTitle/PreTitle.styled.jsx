c6f9a0033f038acb56fd39028c1af40400a4b04d
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-10 20:29:29 +0300

    Update PreTitle.styled.jsx

--- src/components/PreTitle/PreTitle.styled.jsx
+++ src/components/PreTitle/PreTitle.styled.jsx
@@ -4,6 +4,7 @@ import { lightTheme } from '../../utils/theme';
 export const HomeSection = styled.section``;
 
 export const TitleWrap = styled.div`
+ display: inline-block;
  min-width: 151px;
  height: 51px;
  border-radius: 72px;
@@ -21,4 +22,25 @@ export const TitleWrapText = styled.p`
  text-align: center;
  text-transform: capitalize;
  color: ${lightTheme.colors.secondaryColor};
+`;
+
+export const TitleWrapSecondary = styled.div`
+ display: inline-block;
+ min-width: 151px;
+ height: 51px;
+ border-radius: 72px;
+ padding: 11px 40px;
+ background: #7976ff;
+
+ margin-bottom: 17px;
+`;
+
+export const TitleWrapTextSecondary = styled.p`
+ font-family: 'Inter', sans-serif;
+ font-weight: 600;
+ font-size: 18px;
+ line-height: 1.61;
+ text-align: center;
+ text-transform: capitalize;
+ color: #ffffff;
 `;
