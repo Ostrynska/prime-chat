98e23deceb7ed137fc4a434723ef7279fa2fc938
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-11 11:49:32 +0300

    Update Overview.jsx

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,3 +1,4 @@
+import { Container } from '../Container/Conteiner';
 import { ReactSVG } from 'react-svg';
 import {
  Section,
@@ -28,60 +29,62 @@ import app from '../../images/overview/app-image.png';
 const Overview = () => {
  return (
   <Section>
-   <TitleContent>
-    <Content>
-     <PreTitle variant="primary" text={'Platform'} style={{ width: 155 }} />
-     <Title>
-      Get an overview of the <TitleSpan>Primchat</TitleSpan> platform
-     </Title>
-    </Content>
-    <PostTitle>
-     Provide the best, most intuitive support across all channels. Proactively
-     understand customer needs to serve excellence the first time.
-    </PostTitle>
-   </TitleContent>
-   <Platform>
-    <PlatformItem>
-     <ReactSVG src={bussinessMessengerLogo} />
-     <AltText>Powerful Flexible</AltText>
-     <PlatformTitle>Business Messenger</PlatformTitle>
-     <PlatformText>
+   <Container>
+    <TitleContent>
+     <Content>
+      <PreTitle variant="primary" text={'Platform'} style={{ width: 155 }} />
+      <Title>
+       Get an overview of the <TitleSpan>Primchat</TitleSpan> platform
+      </Title>
+     </Content>
+     <PostTitle>
       Provide the best, most intuitive support across all channels. Proactively
-      standerd customer needs serve excellence
-     </PlatformText>
-     <MessengerImage src={messenger} alt="Wellcome" />
-    </PlatformItem>
-    <PlatformItem>
-     <ReactSVG src={managementToolLogo} />
-     <AltText>Smart tools</AltText>
-     <PlatformTitle>Management Tool</PlatformTitle>
-     <PlatformText>
-      Provide the best, most intuitive support across all channels from.
-      Proactively.
-     </PlatformText>
-    </PlatformItem>
-   </Platform>
-   <PlatformRvs>
-    <PlatformItem>
-     <ReactSVG src={customerPlatformLogo} />
-     <AltText>Customer conversation</AltText>
-     <PlatformTitle>Customer Platform</PlatformTitle>
-     <PlatformText>
-      Provide the best, most intuitive support across all chan Proactively
-      standerd customer.
-     </PlatformText>
-    </PlatformItem>
-    <PlatformItem>
-     <ReactSVG src={appLogo} />
-     <AltText>Integration</AltText>
-     <PlatformTitle>Apps & Integrations</PlatformTitle>
-     <PlatformText>
-      Provide the best, most intuitive support across all channels. Proactively
-      standerd customer needs serve excellence.
-     </PlatformText>
-     <AppImage src={app} alt="App" />
-    </PlatformItem>
-   </PlatformRvs>
+      understand customer needs to serve excellence the first time.
+     </PostTitle>
+    </TitleContent>
+    <Platform>
+     <PlatformItem>
+      <ReactSVG src={bussinessMessengerLogo} />
+      <AltText>Powerful Flexible</AltText>
+      <PlatformTitle>Business Messenger</PlatformTitle>
+      <PlatformText>
+       Provide the best, most intuitive support across all channels. Proactively
+       standerd customer needs serve excellence
+      </PlatformText>
+      <MessengerImage src={messenger} alt="Wellcome" />
+     </PlatformItem>
+     <PlatformItem>
+      <ReactSVG src={managementToolLogo} />
+      <AltText>Smart tools</AltText>
+      <PlatformTitle>Management Tool</PlatformTitle>
+      <PlatformText>
+       Provide the best, most intuitive support across all channels from.
+       Proactively.
+      </PlatformText>
+     </PlatformItem>
+    </Platform>
+    <PlatformRvs>
+     <PlatformItem>
+      <ReactSVG src={customerPlatformLogo} />
+      <AltText>Customer conversation</AltText>
+      <PlatformTitle>Customer Platform</PlatformTitle>
+      <PlatformText>
+       Provide the best, most intuitive support across all chan Proactively
+       standerd customer.
+      </PlatformText>
+     </PlatformItem>
+     <PlatformItem>
+      <ReactSVG src={appLogo} />
+      <AltText>Integration</AltText>
+      <PlatformTitle>Apps & Integrations</PlatformTitle>
+      <PlatformText>
+       Provide the best, most intuitive support across all channels. Proactively
+       standerd customer needs serve excellence.
+      </PlatformText>
+      <AppImage src={app} alt="App" />
+     </PlatformItem>
+    </PlatformRvs>
+   </Container>
   </Section>
  );
 };
