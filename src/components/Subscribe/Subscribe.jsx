807138180a643575d3f0a010c70e3fb65b1c1ca4
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-15 23:23:29 +0300

    Create Subscribe.jsx

---
+++ src/components/Subscribe/Subscribe.jsx
@@ -0,0 +1,31 @@
+import { Container } from '../Container/Conteiner';
+import {
+ Section,
+ Title,
+ TitleContent,
+ TitleSpan,
+ Input,
+ Button,
+ Form,
+} from './Subscribe.styled';
+
+const Subscribe = () => {
+ return (
+  <Section>
+   <Container>
+    <TitleContent>
+     <Title>
+      Build stronger with
+      <TitleSpan> Customer relationships</TitleSpan> Primchat
+     </Title>
+     <Form>
+      <Input placeholder="Enter your email" />
+      <Button>Submit</Button>
+     </Form>
+    </TitleContent>
+   </Container>
+  </Section>
+ );
+};
+
+export default Subscribe;
