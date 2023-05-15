c66e5f6572dde49171d37fea03858e9b8dbc5710
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-15 23:23:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { GlobalStyle } from './GlobalStyles';
 
 import HeaderPage from './pages/HeaderPage';
 import HomePage from './pages/HomePage';
+import FooterPage from './pages/Footer.jsx';
 
 function App() {
  return (
   <>
    <HeaderPage />
    <HomePage />
+   <FooterPage />
    <GlobalStyle />
   </>
  );
