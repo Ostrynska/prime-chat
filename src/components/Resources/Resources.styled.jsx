5429544e1ed8fc061bf628cacc77844c3e95aba8
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-15 23:23:37 +0300

    Create Resources.styled.jsx

---
+++ src/components/Resources/Resources.styled.jsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import { lightTheme } from '../../utils/theme';
+
+export const Section = styled.section`
+ padding-top: 135px;
+ padding-bottom: 135px;
+`;
+export const Title = styled.h2`
+ margin: auto;
+ margin-top: 7px;
+ margin-bottom: 90px;
+ max-width: 659px;
+
+ font-family: 'Open Sans', sans-serif;
+ font-weight: 300;
+ font-size: 40px;
+ line-height: 133.2%;
+ /* or 53px */
+
+ text-align: center;
+ text-transform: capitalize;
+ color: ${lightTheme.colors.headerColor};
+`;
+
+export const SpanTitle = styled.span`
+ font-family: 'Open Sans', sans-serif;
+ font-weight: 700;
+`;
+
+export const List = styled.ul`
+ display: flex;
+ flex-direction: column;
+ gap: 150px;
+`;
+
+export const Item = styled.li`
+ display: flex;
+`;
+
+export const ItemContent = styled.div`
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ width: 556px;
+`;
+
+export const ItemTitle = styled.h3`
+ margin-top: 25px;
+ margin-bottom: 28px;
+
+ font-family: 'Open Sans', sans-serif;
+ font-weight: 600;
+ font-size: 30px;
+ line-height: 133.2%;
+ /* or 40px */
+
+ text-transform: capitalize;
+ color: ${lightTheme.colors.headerColor};
+`;
+export const ItemText = styled.p`
+ margin-bottom: 25px;
+
+ font-family: 'Inter', sans-serif;
+ font-style: normal;
+ font-weight: 400;
+ font-size: 20px;
+ line-height: 160%;
+ /* or 32px */
+
+ color: #4e4e4e;
+`;
