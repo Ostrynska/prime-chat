d434da62a9f3c6abd4b3ba38e9274d8c305a84bd
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-15 23:24:38 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,11 @@ import Benefits from '../components/Benefits/Benefits';
 import Overview from '../components/Overview/Overview';
 import Integration from '../components/Integration/Integration';
 import CustomerStories from '../components/CustomerStories/CustomerStories';
+import Resources from '../components/Resources/Resources';
+import Pricing from '../components/Pricing/Pricing';
+import Permission from '../components/Permission/Permission';
+import Solutions from '../components/Solutions/Solutions';
+import Subscribe from '../components/Subscribe/Subscribe';
 // import { Container } from '../components/Container/Conteiner';
 
 const HomePage = () => {
@@ -18,6 +23,11 @@ const HomePage = () => {
    <Overview />
    <Integration />
    <CustomerStories />
+   <Resources />
+   <Pricing />
+   <Permission />
+   <Solutions />
+   <Subscribe />
    {/* </Container> */}
   </>
  );
