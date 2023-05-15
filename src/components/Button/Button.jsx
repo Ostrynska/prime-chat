0c9b0b582270350c399308bf5f11db6584ee2bf1
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-15 23:23:17 +0300

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,25 @@
-import { PrimaryButton, SecondaryButton } from './Button.styled';
+import {
+ PrimaryButton,
+ SecondaryButton,
+ SecondaryButtonIcon,
+} from './Button.styled';
+
+import { ReactComponent as Icon } from '../../images/solutions/btnIcon.svg';
 
 export const Button = ({ text, variant = 'primary' }) => {
  return variant === 'secondary' ? (
   <SecondaryButton>{text}</SecondaryButton>
+ ) : (
+  <PrimaryButton>{text}</PrimaryButton>
+ );
+};
+
+export const ButtonWithIcon = ({ text, variant = 'primary' }) => {
+ return variant === 'icon' ? (
+  <SecondaryButtonIcon>
+   {text}
+   <Icon style={{ marginLeft: '11px' }} />
+  </SecondaryButtonIcon>
  ) : (
   <PrimaryButton>{text}</PrimaryButton>
  );
