74f621e9e09f577d5d929690211106c681f092ca
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-17 14:23:06 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,35 @@
 import { Container } from '../Container/Conteiner';
-import { Section, LogoLink, LogoText } from './Footer.styled';
+import {
+ Section,
+ LogoLink,
+ LogoText,
+ FooterLogoWrap,
+ FooterContactsWrap,
+ FooterContactsItem,
+ FooterContactsLink,
+ FooterContactsSpan,
+ Instagram,
+ Facebook,
+ Twitter,
+ Linkedin,
+ Youtube,
+ UpToTop,
+ FooterWrap,
+ FooterSupportWrap,
+ FooterSupportTitle,
+ FooterSupportList,
+ FooterSupportItem,
+ FooterSupportLink,
+ FooterSocialWrap,
+ FooterSocialTitle,
+ FooterSocialList,
+ FooterSocial,
+ FooterSocialItem,
+ FooterCopyrightWrap,
+ FooterCopyrightText,
+ FooterCopyrightSpan,
+ FooterCopyright,
+} from './Footer.styled';
 
 import { ReactComponent as Logo } from '../../images/logo.svg';
 
@@ -7,71 +37,115 @@ const Footer = () => {
  return (
   <Section>
    <Container>
-    <div>
-     <div>
+    <FooterWrap>
+     <FooterLogoWrap>
       <LogoLink href="#home">
        <Logo />
        <LogoText>Primarily.</LogoText>
       </LogoLink>
-      <div>
-       <address>1811 Silverside Rd, Wilmington, DE 19810, USA</address>
-       <a href="tel:+10012560034">US: +1 (001) 256-0034</a>
-       <a href="tel:+05555141245">UK: +0 (555) 514-1245</a>
-       <a href="mailto:support@primchat.com">email: support@primchat.com</a>
-      </div>
+      <FooterContactsWrap>
+       <FooterContactsItem>
+        <FooterContactsLink href="#">
+         1811 Silverside Rd, Wilmington,
+         <br /> DE 19810, USA
+        </FooterContactsLink>
+       </FooterContactsItem>
+       <FooterContactsItem>
+        <FooterContactsLink href="tel:+10012560034">
+         US: +1 <FooterContactsSpan>(001) 256-0034</FooterContactsSpan>
+        </FooterContactsLink>
+        <br />
+        <FooterContactsLink href="tel:+05555141245">
+         UK: +0 <FooterContactsSpan>(555) 514-1245</FooterContactsSpan>
+        </FooterContactsLink>
+       </FooterContactsItem>
+       <FooterContactsItem>
+        <FooterContactsLink href="mailto:support@primchat.com">
+         email: support@primchat.com
+        </FooterContactsLink>
+       </FooterContactsItem>
+      </FooterContactsWrap>
+     </FooterLogoWrap>
 
-      <div>
-       <div>
-        <h2>Support</h2>
-        <nav>
-         <ul>
-          <li>About</li>
-          <li>Platform</li>
-          <li>Resources</li>
-          <li>Blog</li>
-          <li>Pricing</li>
-         </ul>
-        </nav>
-       </div>
+     <FooterSupportWrap>
+      <FooterSupportTitle>Pages</FooterSupportTitle>
+      <nav>
+       <FooterSupportList>
+        <FooterSupportItem>
+         <FooterSupportLink href="#">About</FooterSupportLink>
+        </FooterSupportItem>
+        <FooterSupportItem>
+         <FooterSupportLink href="#">Platform</FooterSupportLink>
+        </FooterSupportItem>
+        <FooterSupportItem>
+         <FooterSupportLink href="#">Resources</FooterSupportLink>
+        </FooterSupportItem>
+        <FooterSupportItem>
+         <FooterSupportLink href="#">Blog</FooterSupportLink>
+        </FooterSupportItem>
+        <FooterSupportItem>
+         <FooterSupportLink href="#">Pricing</FooterSupportLink>
+        </FooterSupportItem>
+        <FooterSupportItem>
+         <FooterSupportLink href="#">Integration</FooterSupportLink>
+        </FooterSupportItem>
+        <FooterSupportItem>
+         <FooterSupportLink href="#">Testimonials</FooterSupportLink>
+        </FooterSupportItem>
+        <FooterSupportItem>
+         <FooterSupportLink href="#">Permission</FooterSupportLink>
+        </FooterSupportItem>
+        <FooterSupportItem>
+         <FooterSupportLink href="#">Subscribe</FooterSupportLink>
+        </FooterSupportItem>
+       </FooterSupportList>
+      </nav>
+     </FooterSupportWrap>
+    </FooterWrap>
 
-       <div>
-        <h2>Utility Pages</h2>
-        <nav>
-         <ul>
-          <li>Licenses</li>
-          <li>Protected</li>
-          <li>Not Found</li>
-         </ul>
-        </nav>
-       </div>
-      </div>
+    <FooterSocialWrap>
+     <FooterSocial>
+      <FooterSocialTitle>Follow Us:</FooterSocialTitle>
+      <FooterSocialList>
+       <FooterSocialItem>
+        <a href="#home">
+         <Instagram size={40} />
+        </a>
+       </FooterSocialItem>
+       <FooterSocialItem>
+        <a href="#home">
+         <Facebook size={40} />
+        </a>
+       </FooterSocialItem>
+       <FooterSocialItem>
+        <a href="#home">
+         <Twitter size={40} />
+        </a>
+       </FooterSocialItem>
+       <FooterSocialItem>
+        <a href="#home">
+         <Linkedin size={40} />
+        </a>
+       </FooterSocialItem>
+       <FooterSocialItem>
+        <a href="#home">
+         <Youtube size={40} />
+        </a>
+       </FooterSocialItem>
+      </FooterSocialList>
+     </FooterSocial>
 
-      <div>
-       <div>
-        <p>Follow Us :</p>
-        <ul>
-         <li>
-          <a href="#home"></a>
-         </li>
-         <li>
-          <a href="#home"></a>
-         </li>
-         <li>
-          <a href="#home"></a>
-         </li>
-         <li>
-          <a href="#home"></a>
-         </li>
-         <li>
-          <a href="#home"></a>
-         </li>
-        </ul>
-       </div>
-       <div>Copyright © PrimeChat by VictorFlow powered by Webflow.</div>
-       <div>Button</div>
-      </div>
-     </div>
-    </div>
+     <FooterCopyrightWrap>
+      <FooterCopyright>
+       <FooterCopyrightText>
+        Copyright © PrimeChat by{' '}
+        <FooterCopyrightSpan>VictorFlow</FooterCopyrightSpan> powered by{' '}
+        <FooterCopyrightSpan>Webflow</FooterCopyrightSpan>.
+       </FooterCopyrightText>
+       <UpToTop size={50} />
+      </FooterCopyright>
+     </FooterCopyrightWrap>
+    </FooterSocialWrap>
    </Container>
   </Section>
  );
