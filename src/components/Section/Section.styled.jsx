0a445104fa51afcd1412b91d0dce200d7d92b8fe
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-18 14:50:41 +0300

    Create Section.styled.jsx

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { lightTheme } from '../../utils/theme';
+
+export const SectionWrap = styled.section`
+ padding-top: 135px;
+ padding-bottom: 135px;
+`;
+
+export const SectionContent = styled.section`
+ padding-top: 135px;
+ padding-bottom: 135px;
+`;
