c905073613f263af7b1e14c3f8da996d09e47bcc
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-19 12:43:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { GlobalStyle } from './GlobalStyles';
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
 
-import HeaderPage from './pages/HeaderPage';
-import HomePage from './pages/HomePage';
-import FooterPage from './pages/Footer.jsx';
+import { GlobalStyle } from './GlobalStyles';
+import { SharedLayout } from './components/SharedLayout/SharedLayout';
+
+const MainPage = lazy(() => import('./pages/HomePage'));
 
 function App() {
  return (
   <>
-   <HeaderPage />
-   <HomePage />
-   <FooterPage />
+   <Routes>
+    <Route path="/" element={<SharedLayout />}>
+     <Route path="/" element={<MainPage />} />
+    </Route>
+   </Routes>
    <GlobalStyle />
   </>
  );
