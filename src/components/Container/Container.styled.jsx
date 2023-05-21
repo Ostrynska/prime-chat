2c784d7dcf33d7c444b8e481ba6d64570d51acc5
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-21 22:56:48 +0300

    Update Container.styled.jsx

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -3,24 +3,26 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
  margin-left: auto;
  margin-right: auto;
- padding-left: 260px;
- padding-right: 260px;
- width: 1920px;
 
- ${
-  '' /* @media screen and (max-width: ${p => p.theme.breakpoints.mobMax}) {
-  max-width: ${p => p.theme.breakpoints.mob};
+ min-width: 320px;
+ padding-left: 15px;
+ padding-right: 15px;
+ @media screen and (min-width: 480px) {
+  padding: 0 20px;
+  ${'' /* width: 100%; */}
+  width: 480px;
  }
-
- @media screen and (min-width: ${p => p.theme.breakpoints.tab}) {
-  width: ${p => p.theme.breakpoints.tab};
-  padding-left: 32px;
-  padding-right: 32px;
+ @media screen and (min-width: 768px) {
+  padding-left: 140px;
+  padding-right: 140px;
+  width: 768px;
  }
- @media screen and (min-width: ${p => p.theme.breakpoints.desk}) {
-  width: ${p => p.theme.breakpoints.desk};
-  padding-left: 100px;
-  padding-right: 100px;
- } */
+ @media screen and (min-width: 1200px) {
+  padding: 0 120px;
+  min-width: 1200px;
+ }
+ @media screen and (min-width: 1920px) {
+  padding: 0 260px;
+  min-width: 1920px;
  }
 `;
