a2cab185186fb547f3d866c0170a7323059b6951
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-23 15:17:27 +0300

    Update Permission.jsx

--- src/components/Permission/Permission.jsx
+++ src/components/Permission/Permission.jsx
@@ -9,6 +9,8 @@ import {
  ListItem,
  Check,
  CheckText,
+ Image,
+ ImageWrap,
 } from './Permission.styled';
 import { PreTitle } from '../PreTitle/PreTitle';
 import { Container } from '../Container/Conteiner';
@@ -22,43 +24,46 @@ const Permission = () => {
  return (
   <Section id="permission">
    <Container>
-    <Wrap>
-     <PreTitle variant="secondary" text={'Permission'} />
-     <Title>
-      User Role & <TitleSpan>Permissions</TitleSpan>
-     </Title>
-     <Text>
-      Increase engagement and make buying easy with our stylish live chat
-      widget. Provide personalized real-time support customers and visited
-      pages.
-     </Text>
-    </Wrap>
-    <List>
-     <ListItem>
-      <Check />
-      <CheckText>Edit Messenger intro and localization</CheckText>
-     </ListItem>
-     <ListItem>
-      <Check />
-      <CheckText>Edit teammate permissions</CheckText>
-     </ListItem>
-     <ListItem>
-      <Check />
-      <CheckText>Can access people, companies, and account lists</CheckText>
-     </ListItem>
-    </List>
-
-    <img
-     src={isRetina ? permissionImageRetina : permissionImage}
-     alt="Permission"
-     width={877.03}
-     height={692}
+    <div
      style={{
-      position: 'absolute',
-      top: '65px',
-      right: '10px',
+      display: 'flex',
+      gap: '60px',
      }}
-    />
+    >
+     <div style={{ maxWidth: '578px' }}>
+      <Wrap>
+       <PreTitle variant="secondary" text={'Permission'} />
+       <Title>
+        User Role & <TitleSpan>Permissions</TitleSpan>
+       </Title>
+       <Text>
+        Increase engagement and make buying easy with our stylish live chat
+        widget. Provide personalized real-time support customers and visited
+        pages.
+       </Text>
+      </Wrap>
+      <List>
+       <ListItem>
+        <Check />
+        <CheckText>Edit Messenger intro and localization</CheckText>
+       </ListItem>
+       <ListItem>
+        <Check />
+        <CheckText>Edit teammate permissions</CheckText>
+       </ListItem>
+       <ListItem>
+        <Check />
+        <CheckText>Can access people, companies, and account lists</CheckText>
+       </ListItem>
+      </List>
+     </div>
+     <ImageWrap>
+      <Image
+       src={isRetina ? permissionImageRetina : permissionImage}
+       alt="Permission"
+      />
+     </ImageWrap>
+    </div>
    </Container>
   </Section>
  );
