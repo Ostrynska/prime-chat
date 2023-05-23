a0b173afea0fddfa6f0f09c92ca6b334478230f9
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-23 15:17:13 +0300

    Update Subscribe.styled.jsx

--- src/components/Subscribe/Subscribe.styled.jsx
+++ src/components/Subscribe/Subscribe.styled.jsx
@@ -7,13 +7,8 @@ import bgSubscribeRetina from '../../images/subscribe/bgSubscribe@2x.png';
 export const Section = styled.section``;
 
 export const TitleContent = styled.div`
- padding: 128px 96px;
- display: flex;
- align-items: center;
- justify-content: center;
  background-image: url(${bgSubscribe});
  background-repeat: no-repeat;
- width: 1399px;
  height: 361px;
  @media screen and (min-device-pixel-ratio: 2),
   screen and (min-resolution: 192dpi),
@@ -21,19 +16,53 @@ export const TitleContent = styled.div`
   screen and (min-resolution: 2dppx) {
   background-image: url(${bgSubscribeRetina});
  }
+    padding: 23px 34px;
+ width: 100%;
+  @media screen and (min-width: 380px) {
+    padding: 50px 34px;
+ }
+ @media screen and (min-width: 480px) {
+    59px 53px;
+ }
+ @media screen and (min-width: 768px) {
+  padding: 51px 46px;
+  width: 488px;
+ }
+ @media screen and (min-width: 1200px) {
+  padding: 128px 96px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 964px;
+ }
+ @media screen and (min-width: 1920px) {
+  width: 1399px;
+ }
 `;
 
 export const Title = styled.h2`
- width: 558px;
  font-family: 'Open Sans', sans-serif;
  font-weight: 300;
- font-size: 40px;
  line-height: 133.2%;
  /* or 53px */
 
  text-transform: capitalize;
-
  color: #ffffff;
+ margin-bottom: 15px;
+ text-align: center;
+ @media screen and (min-width: 380px) {
+  text-align: start;
+ }
+ @media screen and (min-width: 768px) {
+  width: 100%;
+  font-size: 28px;
+  margin-bottom: 25px;
+ }
+ @media screen and (min-width: 1200px) {
+  width: 558px;
+  font-size: 40px;
+  margin-bottom: 0;
+ }
 `;
 
 export const TitleSpan = styled.span`
@@ -45,9 +74,6 @@ export const Form = styled.form`
 `;
 
 export const Input = styled.input`
- margin-right: 12px;
- width: 368px;
-
  background: #454545;
  border-radius: 72px;
  padding: 27px 37px;
@@ -64,9 +90,19 @@ export const Input = styled.input`
   border: 1px solid #e2992b;
   color: #ffffff;
  }
+ width: 100%;
+ margin-bottom: 8px;
+ @media screen and (min-width: 1200px) {
+  width: 368px;
+  margin-right: 8px;
+ }
+ @media screen and (min-width: 1920px) {
+  margin-right: 12px;
+ }
 `;
 
 export const Button = styled.button`
+ text-align: center;
  padding: 28px 49px 29px;
  width: 168px;
 
