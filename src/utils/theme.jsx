1fcc1b6b2a49737a2084a9ccfc1c08a030d5ef9e
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-24 21:34:03 +0300

    Update theme.jsx

--- src/utils/theme.jsx
+++ src/utils/theme.jsx
@@ -1,23 +1,35 @@
-const mainVars = {
- breakpoints: {
-  mob: '375px',
-  mobMax: '767px',
-  tab: '768px',
-  tabMax: '1439px',
-  desk: '1920px',
- },
+export const mainVars = {
  transitions: {
   main: 'all 250ms cubic-bezier(0.4, 0, 0.2, 1)',
  },
+ colors: {
+  bgColor: '#FFFFFF',
+  bgSecondaryColor: '#FFF3E1',
+  bgGrey: '#ECECEC',
+  contentColor: '#4e4e4e',
+  primaryColor: '#625FFB',
+  secondaryColor: '#E2992B',
+  headerColor: '#303030',
+  titleColor: '#263238',
+  textColor: '#4E4E4E',
+  whiteColor: '#FFFFFF',
+  greyColor: '#F3F3F3',
+  darkGreyColor: '#454545',
+  lightGreyColor: '#F7F7F7',
+  checkOrange: '#FFB545',
+  inputOrange:
+   'linear-gradient(89.88deg, #FFEDD1 1.51%, rgba(255, 255, 255, 0) 84.54%)',
+  checkPurple: '#625FFB',
+  inputPurple:
+   'linear-gradient(89.88deg, #D3D2FF 1.51%, rgba(255, 255, 255, 0) 84.54%)',
+  checkGreen: '#65CB7B',
+  inputGreen:
+   'linear-gradient(89.88deg, #D6F4DC 1.51%, rgba(255, 255, 255, 0) 84.54%)',
+ },
 };
+
 export const lightTheme = {
  ...mainVars,
- //  image: {
- //   imageMob: categoryCenterMob,
- //   imageMobRet: categoryCenterMobRetina,
- //   imageDesk: categoryCenterDesk,
- //   imageDeskRet: categoryCenterDeskRetina,
- //  },
  colors: {
   bgColor: '#FFFFFF',
   bgSecondaryColor: '#FFF3E1',
