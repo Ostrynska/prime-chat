4b26459cc684b3afdc9fd297b8798e0a40d1d9fd
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-05-29 16:28:23 +0300

    Update Chat.jsx

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,43 +1,7 @@
-import io from 'socket.io-client';
-import { useState, useEffect } from 'react';
-
-import { Scrollbars } from 'react-custom-scrollbars';
-import { AvatarGenerator } from 'random-avatar-generator';
-import toast from 'react-hot-toast';
-
+import { useState } from 'react';
 import { Container } from '../Container/Conteiner';
 
-import {
- Section,
- Wrapper,
- NavWrap,
- ChatWrap,
- List,
- Item,
- TitleWrap,
- ChatTitle,
- ChatPreTitle,
- Form,
- OnlineUser,
- OnlineUserList,
- OnlineUserItem,
- InputForm,
- InputFormWrap,
- ChatBot,
- InputName,
- InputText,
- BtnForm,
- ItemWrapper,
- ResetWrap,
- ResetForm,
- OnlineUserItemName,
- ChatBoxList,
- ChatBoxItem,
- ChatDate,
- ChatText,
- ChatUser,
- ChatMessage,
-} from './Chat.styled';
+import { Section, Wrapper, NavWrap, ChatWrap, List, Item } from './Chat.styled';
 import { ReactComponent as Logo } from '../../images/logo.svg';
 import { ReactComponent as Wallet } from '../../images/chat/wallet.svg';
 import { ReactComponent as Swap } from '../../images/chat/swap.svg';
@@ -45,102 +9,14 @@ import { ReactComponent as Chart } from '../../images/chat/chart.svg';
 import { ReactComponent as ChatS } from '../../images/chat/chat.svg';
 import { ReactComponent as Paper } from '../../images/chat/paper.svg';
 
-const socket = {
- current: io('https://chat-back-socket.onrender.com'),
-};
+import UnderConstruction from '../UnderConstruction/UnderConstruction';
+import PrimeChat from './PrimeChat';
 
 const Chat = () => {
- const [onlineUsers, setOnlineUsers] = useState(0);
- const [onlineUsersList, setOnlineUsersList] = useState([]);
- const [user, setUser] = useState('');
- const [message, setMessage] = useState('');
- const [messageList, setMessageList] = useState(
-  JSON.parse(localStorage.getItem('messageList')) || []
- );
- const [usersList, setUsersList] = useState(
-  JSON.parse(localStorage.getItem('usersList')) || []
- );
+ const [selectedItem, setSelectedItem] = useState(null);
 
- const generator = new AvatarGenerator();
- const d = new Date();
-
- useEffect(() => {
-  socket.current.on('changeOnline', size => {
-   setOnlineUsers(size);
-  });
-
-  socket.current.on('chatHistory', data => {
-   setMessageList(data);
-  });
-
-  socket.current.on('onlineUsers', data => {
-   setOnlineUsersList(data);
-  });
-
-  socket.current.on('userJoined', newUser => {
-   setUsersList(prevUsersList => [...prevUsersList, newUser]);
-   setMessageList(prevMessageList => [
-    ...prevMessageList,
-    { name: newUser.name, text: `User ${newUser.name} joined the chat.` },
-   ]);
-  });
-
-  socket.current.on('userLeft', userLeft => {
-   setUsersList(prevUsersList =>
-    prevUsersList.filter(user => user.name !== userLeft.name)
-   );
-   setMessageList(prevMessageList => [
-    ...prevMessageList,
-    { name: userLeft.name, text: `User ${userLeft.name} left the chat.` },
-   ]);
-  });
-
-  return () => {
-   socket.current.off('changeOnline');
-   socket.current.off('chatHistory');
-   socket.current.off('onlineUsers');
-   socket.current.off('userJoined');
-   socket.current.off('userLeft');
-   socket.current.off('alertMessage');
-  };
- }, []);
-
- const handleSubmit = e => {
-  e.preventDefault();
-
-  let today = d.toLocaleString();
-
-  socket.current.emit('addUser', { name: user });
-
-  socket.current.emit('newMessage', { text: message, name: user, date: today });
-
-  addUser(user);
-
-  setMessageList(prevMessageList => [
-   ...prevMessageList,
-   { name: user, text: message, date: today },
-  ]);
-
-  if (message === '' || user === '') {
-   return toast.error("This didn't work.");
-  }
-
-  setMessage('');
-  //   setUser('');
- };
-
- const addUser = name => {
-  const randomAvatar = generator.generateRandomAvatar();
-  const newUser = { name, avatar: randomAvatar };
-  setUsersList(prevUsersList => [...prevUsersList, newUser]);
-
-  setMessageList(prevMessageList => [...prevMessageList]);
-  localStorage.setItem('usersList', JSON.stringify([...usersList, newUser]));
- };
-
- const handleReset = () => {
-  setMessage('');
-  setUser('');
+ const handleItemClick = item => {
+  setSelectedItem(item);
  };
 
  return (
@@ -148,103 +24,27 @@ const Chat = () => {
    <Container>
     <Wrapper>
      <NavWrap>
-      <Logo />
+      <Logo onClick={() => handleItemClick('prime-chat')} />
       <List>
-       <Item>
+       <Item onClick={() => handleItemClick('construction')}>
         <Wallet />
        </Item>
-       <Item>
+       <Item onClick={() => handleItemClick('construction')}>
         <Swap />
        </Item>
-       <Item>
+       <Item onClick={() => handleItemClick('construction')}>
         <Chart />
        </Item>
-       <Item>
+       <Item onClick={() => handleItemClick('construction')}>
         <ChatS />
        </Item>
-       <Item>
+       <Item onClick={() => handleItemClick('construction')}>
         <Paper />
        </Item>
       </List>
      </NavWrap>
      <ChatWrap>
-      <TitleWrap>
-       <ChatTitle>
-        Our <span>Chat</span> List
-       </ChatTitle>
-       <ChatPreTitle>
-        Users online: <span>{onlineUsers}</span>
-       </ChatPreTitle>
-      </TitleWrap>
-      <Form>
-       <Scrollbars style={{ height: '100%', width: 308 }}>
-        <OnlineUser>
-         <OnlineUserList>
-          {usersList.map((user, id) => {
-           const isOnline = onlineUsersList.includes(user.name);
-           return (
-            <OnlineUserItem key={id} online={isOnline}>
-             <ItemWrapper>
-              <img src={user.avatar} alt="User Avatar" width={50} />
-              {/* {isOnline === true ? <span>Online</span> : <span>Ofline</span>} */}
-              <div
-               style={{
-                display: 'flex',
-                alignItems: 'center',
-                marginLeft: '20px',
-               }}
-              >
-               <OnlineUserItemName>{user.name}</OnlineUserItemName>
-              </div>
-             </ItemWrapper>
-            </OnlineUserItem>
-           );
-          })}
-         </OnlineUserList>
-        </OnlineUser>
-       </Scrollbars>
-       <div>
-        <InputFormWrap>
-         <InputForm>
-          <InputName
-           type="text"
-           value={user}
-           placeholder="Enter your name"
-           onChange={e => {
-            setUser(e.currentTarget.value);
-           }}
-          />
-          <InputText
-           type="text"
-           value={message}
-           placeholder="Enter your message"
-           onChange={e => {
-            setMessage(e.currentTarget.value);
-           }}
-          />
-          <BtnForm onClick={handleSubmit}>SUBMIT</BtnForm>
-          <ResetWrap>
-           <ResetForm onClick={handleReset} />
-          </ResetWrap>
-         </InputForm>
-        </InputFormWrap>
-        <ChatBot>
-         <ChatBoxList>
-          {messageList.map((item, _id) => {
-           return (
-            <ChatBoxItem key={item._id}>
-             <ChatMessage>
-              <ChatUser>{item.name}</ChatUser>
-              <ChatText>{item.text}</ChatText>
-             </ChatMessage>
-             <ChatDate>{item.date}</ChatDate>
-            </ChatBoxItem>
-           );
-          })}
-         </ChatBoxList>
-        </ChatBot>
-       </div>
-      </Form>
+      {selectedItem === 'construction' ? <UnderConstruction /> : <PrimeChat />}
      </ChatWrap>
     </Wrapper>
    </Container>
