89c74fe258d2a28eadb6d79a1892e1dcd0f0ef42
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-06-10 10:43:51 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { useLocation } from 'react-router-dom';
+
 import { Container } from '../Container/Conteiner';
 import { Button } from '../Button/Button';
 
@@ -16,48 +18,96 @@ import {
 } from './Header.styled';
 
 const Header = () => {
+ const location = useLocation();
+
+ const isBlogRoute = location.pathname.startsWith('/blog');
+
  return (
-  <Container id="home">
-   <HeaderWrap>
-    <HeaderNav>
-     <LogoLink to={'/'}>
-      <Logo />
-      <LogoText>PrimeChat</LogoText>
-     </LogoLink>
-     <MenuList>
-      <MenuItem>
-       <Link smooth to="/#about-us">
-        About Us
-       </Link>
-      </MenuItem>
-      <MenuItem>
-       <Link smooth to="/#platform">
-        Platform
-       </Link>
-      </MenuItem>
-      <MenuItem>
-       <Link smooth to="/#resources">
-        Resources
-       </Link>
-      </MenuItem>
-      <MenuItem>
-       <Link smooth to="/#solution">
-        Solution
-       </Link>
-      </MenuItem>
-      <MenuItem>
-       <Link smooth to="/#pricing">
-        Pricing
-       </Link>
-      </MenuItem>
-     </MenuList>
-    </HeaderNav>
-    <BtnWrap>
-     <Button variant="secondary" text={'Login'} />
-    </BtnWrap>
-    <BurgerMenu />
-   </HeaderWrap>
-  </Container>
+  <HeaderWrap id="home">
+   <Container>
+    {isBlogRoute ? (
+     <>
+      <HeaderNav>
+       <LogoLink to={'/'}>
+        <Logo />
+        <LogoText>PrimeBlog</LogoText>
+       </LogoLink>
+       <MenuList>
+        <MenuItem>
+         <Link smooth to="/#about-us">
+          About Us
+         </Link>
+        </MenuItem>
+        <MenuItem>
+         <Link smooth to="/#platform">
+          Platform
+         </Link>
+        </MenuItem>
+        <MenuItem>
+         <Link smooth to="/#resources">
+          Resources
+         </Link>
+        </MenuItem>
+        <MenuItem>
+         <Link smooth to="/#solution">
+          Solution
+         </Link>
+        </MenuItem>
+        <MenuItem>
+         <Link smooth to="/#pricing">
+          Pricing
+         </Link>
+        </MenuItem>
+       </MenuList>
+      </HeaderNav>
+      <BtnWrap>
+       <Button variant="secondary" text={'Login'} />
+      </BtnWrap>
+      <BurgerMenu />
+     </>
+    ) : (
+     <>
+      <HeaderNav>
+       <LogoLink to={'/'}>
+        <Logo />
+        <LogoText>PrimeChat</LogoText>
+       </LogoLink>
+       <MenuList>
+        <MenuItem>
+         <Link smooth to="/#about-us">
+          About Us
+         </Link>
+        </MenuItem>
+        <MenuItem>
+         <Link smooth to="/#platform">
+          Platform
+         </Link>
+        </MenuItem>
+        <MenuItem>
+         <Link smooth to="/#resources">
+          Resources
+         </Link>
+        </MenuItem>
+        <MenuItem>
+         <Link smooth to="/#solution">
+          Solution
+         </Link>
+        </MenuItem>
+        <MenuItem>
+         <Link smooth to="/#pricing">
+          Pricing
+         </Link>
+        </MenuItem>
+       </MenuList>
+      </HeaderNav>
+      <BtnWrap>
+       <Button variant="secondary" text={'Login'} />
+      </BtnWrap>
+      <BurgerMenu />
+     </>
+    )}
+   </Container>
+  </HeaderWrap>
  );
 };
 
